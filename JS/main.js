de4c39c2f1c782df8c1a9f1aaec795b655004f58
Vaibhav Kothi <170010116016@adit.ac.in>
2019-10-02 08:23:49 +0530

    Home Page

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
+window.onscroll = function() {
+  windowScroll();
+};
 
+function windowScroll() {
+  var element = document.getElementById("header");
+  if (element.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    element.className = element.className.replace(/\bfixed\b/g, "sticky");
+  } else {
+    element.className = element.className.replace(/\bsticky\b/g, "fixed");
+  }
+}
