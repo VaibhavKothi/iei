808e7e2f4c407c86c68a1371d73091c50e972f0b
Vaibhav Kothi <170010116016@adit.ac.in>
2019-11-08 01:14:31 +0530

    home page complete and team started

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,10 @@ function windowScroll() {
   var element = document.getElementById("header");
   if (element.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     element.className = element.className.replace(/\bfixed\b/g, "sticky");
-  } else {
+    element.style.opacity = "0.4";
+    TweenMax.to(element, 1, {opacity: 1});
+  }
+  else {
     element.className = element.className.replace(/\bsticky\b/g, "fixed");
   }
 }
